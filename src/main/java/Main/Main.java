607d364dbf2d95e714a85011295905f29984462e
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2019-11-21 11:57:34 +0200

    Tekstikäyttöliittymän alustava runko luotu

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,11 +1,17 @@
 package Main;
 
-
-
+import domain.*;
+import ui.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        VinkkiDao dao = new InMemoryVinkkiDao();
+        UI kayttoliittyma = new KonsoliUI(dao);
+        
+        kayttoliittyma.kaynnista();
+        */
         System.out.println("Hello world!");
     }
 

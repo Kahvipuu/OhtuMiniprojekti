be329cf7412f390287afcb795a28242920571a92
Antti Lappalainen <32384162+aflappal@users.noreply.github.com>
2019-11-27 12:58:38 +0200

    Refaktoroitu JSONFileVinkkiDaoa

--- src/main/java/data_access/JSONFileVinkkiDao.java
+++ src/main/java/data_access/JSONFileVinkkiDao.java
@@ -6,22 +6,17 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.List;
 import domain.Kirja;
 import domain.Vinkki;
-import java.util.*;
 
-public class JSONFileVinkkiDao implements VinkkiDao {
+public class JSONFileVinkkiDao extends InMemoryVinkkiDao {
     private String file;
-    private List<Vinkki> vinkit;
     private Gson gson;
 
     public JSONFileVinkkiDao(String file) {
         this.file = file;
-        vinkit = new ArrayList<>();
         gson = new Gson();
 
         try {
@@ -32,7 +27,7 @@ public class JSONFileVinkkiDao implements VinkkiDao {
                     JsonObject obj = element.getAsJsonObject();
                     String tyyppi = gson.fromJson(obj.get("tyyppi"), String.class);
                     if (tyyppi.equals("kirja")) {
-                        vinkit.add(gson.fromJson(obj, Kirja.class));
+                        super.lisaa(gson.fromJson(obj, Kirja.class));
                     }
                 }
             }
@@ -43,22 +38,19 @@ public class JSONFileVinkkiDao implements VinkkiDao {
     }
 
     @Override
-    public List<Vinkki> listaaKaikki() {
-        return vinkit;
+    public void lisaa(Vinkki uusiVinkki) {
+        super.lisaa(uusiVinkki);
+        kirjoitaJson();
     }
 
-    @Override
-    public void lisaa(Vinkki uusiVinkki) {
-        vinkit.add(uusiVinkki);
-
-        String json = gson.toJson(vinkit);
+    private void kirjoitaJson() {
+        String json = gson.toJson(this.listaaKaikki());
         try{
-            FileWriter kirjoittaja = new FileWriter(file);
+            FileWriter kirjoittaja = new FileWriter(this.file);
             kirjoittaja.write(json);
             kirjoittaja.close();
         } catch(Exception e) {
             System.out.println(e);
         }
     }
-
 }
